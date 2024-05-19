43a032bb36c4e4c2c190a92b2b4d28691440372e
mejor0108 <mauricio@jorquera.com.ar>
2024-05-19 20:19:16 -0300

    new design class weather and new class HoraData

--- weather-cmd/ConnectMongoDB/model/weatherModel.py
+++ weather-cmd/ConnectMongoDB/model/weatherModel.py
@@ -1,129 +1,72 @@
-from mongoengine import Document, DateField, StringField, FloatField, DateTimeField, connect
-from datetime import datetime
+from mongoengine import Document, DateField, StringField, FloatField, DateTimeField, connect, EmbeddedDocument, fields
+import datetime
 from dotenv import load_dotenv
 import os
 
 
 
 
+class HoraData(EmbeddedDocument):
+    """
+    Represents weather data for a specific hour.
+
+    Attributes:
+        hora (DateTimeField): The timestamp of the weather data.
+        temperatura (FloatField): The temperature in Celsius.
+        estado_nuboso (StringField): The description of the cloud state.
+        visibilidad (FloatField): The visibility in kilometers.
+        sensacion_termica (FloatField): The perceived temperature in Celsius.
+        humedad_relativa (FloatField): The relative humidity in percentage.
+        viento_intensidad (FloatField): The wind intensity in kilometers per hour.
+        viento_direccion (StringField): The wind direction.
+        presion_superfice (FloatField): The surface pressure in hPa.
+    """
+
+    hora = fields.StringField(required=True, regex=r'^\d{2}:\d{2}(:\d{2})?$')  # Formato HH:MM o HH:MM:SS
+    temperatura = fields.FloatField(required=True)
+    estado_nuboso = fields.StringField(required=True)
+    visibilidad = fields.FloatField(required=True, min_value=0)
+    sensacion_termica = fields.FloatField(required=True)
+    humedad_relativa = fields.FloatField(required=True, min_value=0)
+    viento_intensidad = fields.FloatField(required=True, min_value=0)
+    viento_direccion = fields.StringField(required=True)
+    presion_superfice = fields.FloatField(required=True, min_value=0)
+
+    def __init__(self, *args, **kwargs):
+        super(HoraData, self).__init__(*args, **kwargs)
+
+    def __str__(self):
+        return (
+            'hora :' + str(self.hora) + '\n' +
+            'temperatura :' + str(self.temperatura) + '\n' +
+            'estado nuboso :' + str(self.estado_nuboso) + '\n' +
+            'visibilidad :' + str(self.visibilidad) + '\n' +
+            'sensacion termica :' + str(self.sensacion_termica) + '\n' +
+            'humedad relativa :' + str(self.humedad_relativa) + '\n' +
+            'viento intensidad :' + str(self.viento_intensidad) + '\n' +
+            'viento direccion :' + str(self.viento_direccion) + '\n' +
+            'presion superficie :' + str(self.presion_superfice) + '\n'
+        )
+
+   
 
 class Weather(Document):
     # fecha = None                       # date
     # estacion = None                    # str
-    # hora = None                        # timestamp
-    # temperatura = None                 # float
-    # estado_nuboso = None               # str
-    # visibilidad = None                 # float > 0
-    # sensacion_termica = None           # float 
-    # humedad_relativa = None            # float >= 0
-    # viento_intensidad = None           # float >= 0
-    # viento_direccion = None            # str
-    # presion_superfice = None           # float > 0 
+    # List [ HoraDate ]           
+    # 
     
-    fecha_hora          = DateField(required=True)
+    fecha               = DateField(required=True)
     estacion            = StringField(required=True)
-    temperatura         = FloatField(required=True)
-    estado_nuboso       = StringField(required=True)
-    visibilidad         = FloatField(min_value=0, required=True)
-    sensacion_termica   = FloatField(required=True)
-    humedad_relativa    = FloatField(min_value=0, required=True)
-    viento_intensidad   = FloatField(min_value=0, required=True)
-    viento_direccion    = StringField(required=True)
-    presion_superfice   = FloatField(min_value=0, required=True)
+    datos_hora          = fields.EmbeddedDocumentListField(HoraData) 
+    
+    
+
     
     def __init__(self, *args ,**kwargs):
         # insertar los parametro en su correspondiente atributo
         super(Weather, self).__init__(*args, **kwargs)
        
-    # @property
-    # def fecha(self):
-    #     return self._fecha_hora
-
-    # @fecha.setter
-    # def fecha(self, value):
-    #     self._fecha_hora = value
-
-    # @property
-    # def estacion(self):
-    #     return self._estacion
-
-    # @estacion.setter
-    # def estacion(self, value):
-    #     self._estacion = value
-
-    # @property
-    # def hora(self):
-    #     return self._hora
-
-    # @hora.setter
-    # def hora(self, value):
-    #     self._hora = value
-
-    # @property
-    # def temperatura(self):
-    #     return self.temperatura
-
-    # @temperatura.setter
-    # def temperatura(self, value):
-    #     self._temperatura = value
-
-    # @property
-    # def estado_nuboso(self):
-    #     return self._estado_nuboso
-
-    # @estado_nuboso.setter
-    # def estado_nuboso(self, value):
-    #     self._estado_nuboso = value
-
-    # @property
-    # def visibilidad(self):
-    #     return self._visibilidad
-
-    # @visibilidad.setter
-    # def visibilidad(self, value):
-    #     self._visibilidad = value
-
-    # @property
-    # def sensacion_termica(self):
-    #     return self._sensacion_termica
-
-    # @sensacion_termica.setter
-    # def sensacion_termica(self, value):
-    #     self._sensacion_termica = value
-
-    # @property
-    # def humedad_relativa(self):
-    #     return self._humedad_relativa
-
-    # @humedad_relativa.setter
-    # def humedad_relativa(self, value):
-    #     self._humedad_relativa = value
-
-    # @property
-    # def viento_intensidad(self):
-    #     return self._viento_intensidad
-
-    # @viento_intensidad.setter
-    # def viento_intensidad(self, value):
-    #     self._viento_intensidad = value
-
-    # @property
-    # def viento_direccion(self):
-    #     return self._viento_direccion
-
-    # @viento_direccion.setter
-    # def viento_direccion(self, value):
-    #     self._viento_direccion = value
-
-    # @property
-    # def presion_superfice(self):
-    #     return self._presion_superfice
-
-    # @presion_superfice.setter
-    # def presion_superfice(self, value):
-    #     self._presion_superfice = value
-        
-    
     
     
 
@@ -170,23 +113,50 @@ if __name__ == '__main__':
     
     connect(os.getenv('MONGODB_DATABASE'),alias='core', host=os.getenv('MONGODB_SERVER'))
     
-    weather = Weather( 
-        fecha_hora='2021-10-01',
-        estacion='Aeropuerto de Barcelona',
-        temperatura=20.0,
-        estado_nuboso='Despejado',
+    
+    
+    
+    
+    
+    
+#     Crear una instancia del subdocumento
+    hora_data_18 = HoraData(
+        hora="18:00",
+        temperatura=25.5,
+        estado_nuboso='Parcialmente nublado',
         visibilidad=10.0,
-        sensacion_termica=20.0,
-        humedad_relativa=50.0,
-        viento_intensidad=10.0,
-        viento_direccion='Norte',
-        presion_superfice=1013.0
+        sensacion_termica=26.0,
+        humedad_relativa=60.0,
+        viento_intensidad=15.0,
+        viento_direccion='Noreste',
+        presion_superfice=1012.0
     )
+    hora_data_19 = HoraData(
+        hora="19:00",  # Año, mes, día, hora, minuto
+        temperatura=26.5,
+        estado_nuboso='',
+        visibilidad=10.0,
+        sensacion_termica=26.0,
+        humedad_relativa=60.0,
+        viento_intensidad=15.0,
+        viento_direccion='Noreste',
+        presion_superfice=1012.0
+    )
+
+
+    # Crear una instancia del documento principal
+    estacion_data_example = Weather(
+        fecha=datetime.date(2024, 5, 19),
+        estacion='Estacion Central',
+        datos_hora=[hora_data_18,hora_data_19]  # Puedes agregar más instancias de HoraData si lo necesitas
+    )
+
+
+
     
-    weather.save()
+    estacion_data_example.save()
     
-    print(weather)
-    
+    print(estacion_data_example)
     
     
     
